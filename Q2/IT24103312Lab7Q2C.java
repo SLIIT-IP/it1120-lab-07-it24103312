88bc8ba116a45540e5ce53d3216a99e7c748c1da
it24103312 <it24103312@my.sliit.lk>
2024-09-18 19:34:18 +0530

    add IT24103312Lab7Q2C.java to  Q2C

---
+++ Q2/IT24103312Lab7Q2C.java
@@ -0,0 +1,12 @@
+public class IT24103312Lab7Q2C {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
